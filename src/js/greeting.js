afff6d67996d37a317bf8f34de7a01ba28f7d690
onjwon <141843113+onjwon@users.noreply.github.com>
2024-02-03 13:26:41 +0900

    Create greeting.js

---
+++ src/js/greeting.js
@@ -0,0 +1,29 @@
+"use strick";
+
+history.pushState(null, null, location.href);
+window.onpopstate = function () {
+  history.go(1);
+};
+
+const greeting = document.querySelector(".greeting-text span");
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input");
+
+// 1. input value를 local storage에 저장하기
+function onLoginSubmit() {
+  const usernameInput = loginInput.value;
+  localStorage.setItem("username", usernameInput);
+  paintGreeting(usernameInput);
+}
+// 2. local storage에서 get item 해오기
+function paintGreeting(username) {
+  greeting.innerText = `${username}, Happy day`;
+}
+
+savedUsername = localStorage.getItem("username");
+
+if (savedUsername === null) {
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
