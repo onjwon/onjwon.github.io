901f1b5976e35ae9ab58849f88e381541c1d7916
onjwon <141843113+onjwon@users.noreply.github.com>
2024-02-04 23:54:17 +0900

    Update weather.js

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,6 +1,7 @@
-const weather = document.querySelector("#weather span:first-child");
-const city = document.querySelector("#weather span:last-child");
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+ const city = document.querySelector("#weather span:first-child");
+ const weather = document.querySelector("#weather span.weth");
+ const temp = document.querySelector("#weather span:last-child");
+ const API_KEY = "d67b4647ec6e3bf94e153cfda06b2a47";
 
 function onGeoOk(position) {
     console.log(position);
@@ -8,12 +9,14 @@ function onGeoOk(position) {
    const lng = position.coords.longitude;
    console.log("You live in", lat, lng);
    const lon = position.coords.longitude;
-   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+   //console.log(url);
    fetch(url)
      .then((response) => response.json())
      .then((data) => {
        city.innerText = data.name;
-       weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+       weather.innerText = `${data.weather[0].main}`;
+       temp.innerText = `${data.main.temp}`;
      });
 }
 function onGeoError() {
